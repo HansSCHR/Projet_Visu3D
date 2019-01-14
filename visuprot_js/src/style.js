40a789d0733580e15cef95ed5491ae1f67515578
coddy33 <nanouwow@hotmail.fr>
2019-01-14 14:32:43 +0100

    ready to branch

---
+++ visuprot_js/src/style.js
@@ -0,0 +1,16 @@
+
+
+// $("#viewport").
+
+// $( document ).ready(function() {
+//     w = $( "#viewport" ).width();
+//     console.log("coucou", w.toString())
+//     $( "#viewport" ).css("width", w.toString() )
+// });
+
+
+
+
+$('#viewport').hover(function() {
+    $(this).css('cursor','crosshair');
+});
